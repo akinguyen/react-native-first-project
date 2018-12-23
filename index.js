f59588aa3c10dc11d8b6457f1c0babdce663b8d2
thaominh123 <36982208+thaominh123@users.noreply.github.com>
2018-12-22 16:46:22 -0800

    Update index.js

--- index.js
+++ index.js
@@ -4,19 +4,3 @@
 import App from "./App";
 import { name as appName } from "./app.json";
 
-//import { Provider } from "react-redux";
-//import store from "./src/storeConfig";
-
-//import React, { Component } from "react";
-/*
-class AppEntry extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <App />
-      </Provider>
-    );
-  }
-}
-*/
-//AppRegistry.registerComponent(appName, () => App);
